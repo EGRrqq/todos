a4f6e82c290fc997ec815d492f12665ffeab4373
EGRrqq <egrrqqdev@gmail.com>
2024-04-18 09:57:16 -0400

    feat: remove state stuff forgot about it :\

--- src/components/TodoForm/TodoAction.tsx
+++ src/components/TodoForm/TodoAction.tsx
@@ -5,22 +5,5 @@ interface ITodoAction {
 }
 
 export function TodoAction({ children }: ITodoAction) {
-  const [value, setValue] = useState<string>("");
-  // input value stuff
-  const saveInputValue = (e: FormEvent<HTMLInputElement>) =>
-    setValue((e.target as HTMLInputElement).value);
-
-  const resetInputValue = () => setValue("");
-
-  // todos stuff
-  function addTodos(e: FormEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    setTodos([...todos, new Todo(value)]);
-  }
-  function handleTodos(e: FormEvent<HTMLButtonElement>) {
-    addTodos(e);
-    resetInputValue();
-  }
-
   return <>{children}</>;
 }
